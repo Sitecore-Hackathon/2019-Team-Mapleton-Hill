3cb463743050fe509ff676d0cd917fd6177236f9
Ehren Dames <ehren@mapletonhill.net>
2019-03-02 01:30:07 -0700

    feat(Image): add web image service for downloading

---
+++ src/Project/Common/code/Services/IWebImageService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.Web.Services
+{
+    public interface IWebImageService
+    {
+        string Download(string imageUrl, string directoryDownload);
+    }
+}
---
+++ src/Project/Common/code/Services/WebImageService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+
+namespace Common.Web.Services
+{
+    public class WebImageService : IWebImageService
+    {
+        public string Download(string imageUrl, string directoryDownload)
+        {
+            Uri imageUri;
+            if(!Uri.TryCreate(imageUrl, UriKind.Absolute, out imageUri))
+            {
+                Sitecore.Diagnostics.Log.Error($"Could not download image {imageUrl} because it is not a valid web address", this);
+                return string.Empty;
+            }
+            
+            string fileName = System.IO.Path.GetFileName(imageUrl);
+            string savePath = $"{directoryDownload}\\{fileName}";
+            try
+            {
+                WebClient client = new WebClient();
+                client.DownloadFile(imageUrl, savePath);
+            }
+            catch (WebException ex)
+            {
+                Sitecore.Diagnostics.Log.Error($"Could not download image {imageUrl}", ex, this);
+            }
+            return savePath;
+        }
+    }
+}
