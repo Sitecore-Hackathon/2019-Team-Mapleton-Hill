ee3363759beecde68a7509ef22969afe8b078816
Ehren Dames <ehren@mapletonhill.net>
2019-03-01 23:55:40 -0700

    feat(Commandlet): add skeleton of a commandlet

---
+++ src/Project/Common/code/Commandlets/GetImageData.cs
@@ -0,0 +1,25 @@
+﻿using Cognifide.PowerShell.Commandlets;
+using Sitecore.Diagnostics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management.Automation;
+using System.Web;
+
+namespace Common.Web.Commandlets
+{
+    [Cmdlet("Import", "ImagesFromDataFile")]
+    public class GetImageData : BaseCommand
+    {
+
+        [Parameter(Mandatory = true)]
+        [ValidateNotNullOrEmpty]
+        public string DataPath { get; set; }
+
+        protected override void ProcessRecord()
+        {
+            Log.Info(DataPath, this);
+            base.ProcessRecord();
+        }
+    }
+}
